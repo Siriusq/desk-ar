4688fdb1996be6717a32fda34a4591da541f34e5
Siriusq <eargon.skyle@gmail.com>
2025-10-16 17:49:43 +0800

    Import Brite from Bootswatch and update welcome page's UI

--- src/pages/WelcomePage.vue
+++ src/pages/WelcomePage.vue
@@ -1,17 +1,14 @@
 <template>
-  <div class="d-flex align-items-center text-bg-dark min-vh-100">
-    <div class="cover-container w-100 p-3 mx-auto text-center">
-      <h1>欢迎使用 DeskAR</h1>
+  <div class="d-flex align-items-center text-bg-light min-vh-100">
+    <div class="welcome-container w-100 p-3 mx-auto text-center">
+      <h1 class="mb-4">欢迎使用 DeskAR</h1>
 
       <div class="d-flex flex-column flex-md-row justify-content-center gap-3">
-        <button
-          class="btn btn-lg btn-light fw-bold border-white bg-white"
-          @click="$router.push('/main')"
-        >
+        <button class="btn btn-lg btn-primary fw-bold w-auto" @click="$router.push('/main')">
           创建新布局
         </button>
 
-        <button class="btn btn-lg btn-outline-light fw-bold" @click="$router.push('/main')">
+        <button class="btn btn-lg btn-warning fw-bold w-auto" @click="$router.push('/main')">
           从文件打开
         </button>
       </div>
@@ -20,24 +17,10 @@
 </template>
 
 <style scoped>
-.welcome {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-}
 button {
-  margin-top: 20px;
   padding: 10px 20px;
 }
-.cover-container {
-  max-width: 42em;
-}
-.btn-light,
-.btn-light:hover,
-.btn-light:focus {
-  color: #333;
-  text-shadow: none; /* Prevent inheritance from `body` */
+.welcome-container {
+  max-width: 32em;
 }
 </style>
