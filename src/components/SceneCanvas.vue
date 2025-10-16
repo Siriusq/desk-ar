af10aa7b55a48c0fca9fe5207546916f522a1d71
Siriusq <eargon.skyle@gmail.com>
2025-10-16 19:58:48 +0800

    Add Test Scene

---
+++ src/components/SceneCanvas.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { TresCanvas } from '@tresjs/core'
+import { OrbitControls } from '@tresjs/cientos'
+</script>
+
+<template>
+  <TresCanvas clear-color="#82DBC5" window-size>
+    <TresPerspectiveCamera :position="[7, 7, 7]" :look-at="[0, 0, 0]" />
+    <!-- The Donut -->
+    <TresMesh :position="[0, 2, 0]">
+      <TresTorusGeometry :args="[1, 0.4, 16, 32]" />
+      <TresMeshBasicMaterial color="#ff6b35" />
+    </TresMesh>
+
+    <!-- Visual Helpers -->
+    <TresAxesHelper />
+    <TresGridHelper />
+    <OrbitControls />
+  </TresCanvas>
+</template>
